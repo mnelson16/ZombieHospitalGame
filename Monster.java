b389f0b82f96612669f68aa1d3876ab1626b2dc8
jltimmer <jltimmer@users.noreply.github.com>
2016-10-10 14:33:17 -0400

    Monster
    Monster file

---
+++ Monster.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+
+/**Class: Monster.java
+ * @author Janna Timmer
+ * @version 1.0
+ * Course: ITEC 2150 Spring 2016
+ * Written: Oct 10, 2016
+ * 
+ *
+ * This class - now describe what the class does
+ *
+ * Purpose: - Describe the purpose of this class
+ */
+
+public class Monster
+{
+	private String monsterID;
+	private int health, attack, defense;
+	private ArrayList<Artifact> inventory = new ArrayList<Artifact>();
+	
+	public Monster(String ID, int maxHealth, int atk, int def)
+	{
+		monsterID = ID;
+		health = maxHealth;
+		attack = atk;
+		defense = def;
+	}
+	
+	
+}
