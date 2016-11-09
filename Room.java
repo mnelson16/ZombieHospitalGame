a7868ed9ac8dee53cd3c19ae2b32382cb234db9c
Mxiong3 <mxiong3@ggc.edu>
2016-11-09 15:56:55 -0500

    Add files via upload

--- Room.java
+++ Room.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 /**Class: Room
  * @authors Janna Timmer, Matthew Nelson, Matthew Xiong
@@ -17,26 +19,57 @@ public class Room
 	private String roomID, description;
 	private ArrayList<Monster> monsters = new ArrayList<Monster>();
 	private Puzzle puzzle;
-	private ArrayList<String> exitIDs = new ArrayList<String>();
+	private Map<String,Room> exitIDs;
 	private boolean saveableRoom;
 	
 	/**
+	 * @param roomID 
+	 * @param description 
+	 * @param exitIDs 
+	 * @param saveableRoom 
+	 * @param monsters 
+	 * @param monsters 
+	 * @param puzzle 
+	 * @param puzzle 
 	 * 
 	 */
-	public Room(String roomID, String description, ArrayList<Monster> monsters, Puzzle puzzle,
-			ArrayList<String> exitIDs, boolean saveableRoom)
+	public Room(String roomID, String description, Map<String, Room> exitIDs, boolean saveableRoom, ArrayList<Monster> monsters, Puzzle puzzle)
 	{
 		this.roomID = roomID;
 		this.description = description;
-		this.monsters = monsters;
-		this.puzzle = puzzle;
 		this.exitIDs = exitIDs;
 		this.saveableRoom = saveableRoom;
+		this.monsters= monsters;
+		this.puzzle = puzzle;
+		exitIDs = new LinkedHashMap<String, Room>();
 	}
-	
-	public void goDirection()
-	{
-		
+	public void setExits(Room north, Room east, Room south, Room west) {
+		if (north != null)
+			exitIDs.put("north", north);
+		if (east != null)
+			exitIDs.put("east", east);
+		if (south != null)
+			exitIDs.put("south", south);
+		if (west != null)
+			exitIDs.put("west", west);
+	}
+	public String getRoomID() {
+		return roomID;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public ArrayList<Monster> getMonsters() {
+		return monsters;
+	}
+	public Puzzle getPuzzle() {
+		return puzzle;
+	}
+	public Map<String, Room> getExitIDs() {
+		return exitIDs;
+	}
+	public boolean isSaveableRoom() {
+		return saveableRoom;
 	}
 
-}
+	}
