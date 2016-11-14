890baa6ca08175c359026674968bfe6f51949f4d
mnelson16 <mnelson16@ggc.edu>
2016-11-14 14:14:47 -0500

    Update PuzzleTester.java

--- PuzzleTester.java
+++ PuzzleTester.java
@@ -8,10 +8,10 @@ public class PuzzleTester
 	{
 		// TODO Auto-generated method stub
 		PuzzleFactory pf = new PuzzleFactory();
-		Puzzle puzzle = pf.getPuzzles().get("PUZ003");
+		Puzzle puzzle = pf.getPuzzles().get("PUZ006");
 		puzzle.encounterPuzzle();
 		HashMap<String, Artifact> inventory = new HashMap<String, Artifact>();
-		Player player = new Player("0", 10, 5, 5, false, "1");
+		Player player = new Player("0", 10, 5, 5, "1");
 		Scanner input = new Scanner(System.in);
 		
 		while(!puzzle.getSolved())
