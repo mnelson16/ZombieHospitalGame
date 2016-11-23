fd4b88a00f157a6d7e46b2c393e16f6ddac8e7fb
jltimmer <jltimmer@users.noreply.github.com>
2016-11-22 19:16:20 -0500

    Update ZHTester.java

--- ZHTester.java
+++ ZHTester.java
@@ -69,7 +69,7 @@ public class ZHTester
 			//Two or more word commands
 			else if (command.equals("use") || command.equals("observe") || command.equals("stats"))
 			{
-				cs.CSRun(player);	
+				player = cs.CSRun(player);	
 			}
 			else if(command.equals("go"))
 			{
