b796bb0edb4605bc5eafaad74ba076b351af831b
mnelson16 <mnelson16@ggc.edu>
2016-12-02 12:13:49 -0500

    Update ZHTester2.java

--- ZHTester2.java
+++ ZHTester2.java
@@ -1,16 +1,21 @@
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
 import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.TextArea;
+import javafx.stage.Stage;
 
 /**Class: ZHTester
  * @authors Janna Timmer, Matthew Nelson, Matthew Xiong
  * @version 1.0
  * Course: ITEC 3860 Fall 2016
  * Written: Oct 3, 2016
- * 
+ *
  *
  * This class handles user input and organizes/combines subsystem components.
  *
@@ -22,7 +27,8 @@ public class ZHTester
 	private static String command, object, userInput;
 	private static Player player;// = new Player("00", 10, 10, 0, null);
 	private static HashMap<String, Room> rooms;
-	public static File stockSave = new File("stockSave.dat"), save1 = new File("save1.dat"), 
+	private static HashMap<String, Artifact> artifacts;
+	public static File stockSave = new File("stockSave.dat"), save1 = new File("save1.dat"),
 			save2 = new File("save2.dat"), save3 = new File("save3.dat");
 
 
@@ -31,84 +37,100 @@ public class ZHTester
 	 */
 	public static void main(String[] args)
 	{
-		Zombie tempZom = new Zombie("M01", 50, 1, null, "Patient Zombie", "Standard attack", 0, 0);
 		Scanner input = new Scanner(System.in);
+		ZombieFactory.createZombies();
+		ArtifactFactory.createArtifacts();
+		PuzzleFactory.createPuzzles();
+		ArtifactFactory af = new ArtifactFactory();
 		GameSubsystem gs = new GameSubsystem();
 		CharacterSubsystem cs = new CharacterSubsystem();
 		RoomSubsystem rs = new RoomSubsystem();
 		player = gs.game.getNewPlayer();
 		rooms = gs.game.getRooms();
-		boolean monster_in_room = true;
-		//Temporary - inventory should be added to in... room?
-
-		//Temp
-		String help = "Available commands: \n"
-				+ "Fight, Go <Direction>, Help, Inventory, Observe <Item>, Stats, Use <Item>";
-		System.out.println(help);
+		artifacts = af.getAllArtifacts();
 		gs.game.makeNewGame();
 
+		String help = "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n" +
+				"|--Help Commands--													|"
+				+ "\n|Help (Displays available commands and current room exits.)								|"
+				+ "\n|--Game Commands-- 													|"
+				+ "\n|New || Save || Load													|"
+				+ "\n|--Other Commands-- 													|"
+				+ "\n|Go <Direction> || Inventory || Observe <Item> || Stats || Take <Item> || Use <Item>					|" +
+				"\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~";
+		System.out.println(gs.game.getZHText());
+		System.out.println(help + "\n");
+
 		System.out.println(rooms.get(player.getCurrentRoomID()).getDescription());
+		rooms.get(player.getCurrentRoomID()).setDescription("This is where you woke up. "
+				+ "There's nothing interesting to find here.");
+			while (true)
+			{
+				try
+				{
+					if(player.getHealth() <= 0)
+					{
+						System.out.println("You died.. \nStarting new game!");
+						gs.game.makeNewGame();
+						gs.game.newGame();
+					}
+					System.out.print("> ");
+					userInput = input.nextLine().toLowerCase();
+					userInput = userInput.replaceAll(" ", "_");
+					System.out.print("\n");
 
-		if (monster_in_room)
-		{
+					if (userInput.contains("_")) //If userInput is multi-word
+					{
+						command = userInput.substring(0, userInput.indexOf("_"));
+						//Set object to word(s) after command with underscores replacing spaces
+						object = userInput.substring(userInput.indexOf("_") + 1);
+					}
+					else //If userInput is one word
+					{
+						command = userInput;
+						object = "";
+					}
 
-		}
 
-		/*
-		 * "take" command go to room?
-		 */
-
-		while (true)
-		{
-			System.out.print("> ");
-			userInput = input.nextLine().toLowerCase();
-			userInput = userInput.replaceAll(" ", "_");
-
-			if (userInput.contains("_")) //If userInput is multi-word
-			{
-				command = userInput.substring(0, userInput.indexOf("_"));
-				//Set object to word(s) after command with underscores replacing spaces
-				object = userInput.substring(userInput.indexOf("_") + 1);
+					if (command.equals("help"))
+					{
+						System.out.println(help);
+						if (rs.getActiveRoom() != null)
+						{
+							System.out.println("\n" + rs.getActiveRoom().getExits());
+						}
+					}
+					else if (command.equals("use") || command.equals("observe") || command.equals("stats")
+							|| command.equals("inventory") || command.equals("take"))
+					{
+						cs.CSRun(player, rooms, command, object);
+					}
+					else if(command.equals("go"))
+					{
+						rs.RSRun(player, rooms, command, object, artifacts);
+					}
+					else if(command.equals("new") || command.equals("save") || command.equals("load"))
+					{
+						gs.GSRun(player, rooms, command, object);
+					}
+					else if(command.equals("exit"))
+					{
+						System.exit(0);
+					}
+				}
+				catch(Exception e)
+				{
+					System.out.println("What is it you want to do?");
+				}
 			}
-			else //If userInput is one word
-			{
-				command = userInput;
-				object = "";
-			}
-			
-			//Temp, move help functionality to Game class
-			if (command.equals("help"))
-			{
-				System.out.println(help);
-			}
-			else if (command.equals("use") || command.equals("observe") || command.equals("stats")
-					|| command.equals("inventory") || command.equals("fight") || command.equals("take"))
-			{
-				cs.CSRun(player, rooms, command, object, tempZom);
-			}
-			else if(command.equals("go"))
-			{
-				rs.RSRun(player, rooms, command, object);
-			}
-			else if(command.equals("new") || command.equals("save") || command.equals("load"))
-			{
-				gs.GSRun(player, rooms, command, object);
-			}
-			else 
-			{
-				System.out.println("What is it you want to do?");
-			}
-
-			//System.out.println();
-		}
 	}
 
-	public Player getPlayer()
+	public static Player getPlayer()
 	{
 		return player;
 	}
 
-	public HashMap<String, Room> getRooms()
+	public static HashMap<String, Room> getRooms()
 	{
 		return rooms;
 	}
