18ad7018b96cfb45f3a17a99bc252ad0e7a6a32b
mnelson16 <mnelson16@ggc.edu>
2016-12-02 12:05:09 -0500

    Update CharacterSubsystem2.java

--- CharacterSubsystem2.java
+++ CharacterSubsystem2.java
@@ -1,17 +1,16 @@
 import java.util.HashMap;
 import java.util.LinkedHashMap;
-import java.util.Scanner;
 
 /**Class: CharacterSubsystem
  * @author Janna Timmer, Matthew Nelson, Matthew Xiong
  * @version 1.0
  * Course: ITEC 3860 Fall 2016
  * Written: Nov 7, 2016
- * 
  *
- * This class - now describe what the class does
  *
- * Purpose: - Describe the purpose of this class
+ * This class provides structure for the Character subsystem (Character and Artifact classes)
+ *
+ * Purpose: to organize the Character subsystem
  */
 
 
@@ -27,10 +26,10 @@ public class CharacterSubsystem
 	 * @param command -- first word user typed in (game action to be taken)
 	 * @param activeArtifact -- artifact user typed in (null if user typed in one word command)
 	 */
-	public void CSRun(Player p, HashMap<String, Room> rooms, String command, String artifactInput, Zombie z)
+	public void CSRun(Player p, HashMap<String, Room> rooms, String command, String artifactInput)
 	{
 		Artifact activeArtifact;
-		
+
 		player = p;
 		activeRoom = rooms.get(player.getCurrentRoomID());
 
@@ -42,27 +41,21 @@ public class CharacterSubsystem
 		{
 			System.out.println(player.inventoryToString());
 		}
-		else if (command.equals("fight"))
-		{
-			fight(z);
-		}
 		else if(command.equals("take"))
 		{
-			System.out.println(activeRoom.getArtifact().getName());
 			activeArtifact = parseArtifactName(artifactInput);
 			//If the item is in the room
-			if(!(activeRoom.getArtifact() == null))
+			if(activeRoom.getArtifact() != null)
 			{
 				LinkedHashMap<String, Artifact> newInv = player.getPlayerInventory();
 				newInv.put(activeArtifact.getName(), activeArtifact);
 				player.setPlayerInventory(newInv);
-				activeRoom.setArtifact(null);
-				System.out.println("You got the " + activeArtifact.getName() + "! \n" + 
-				"The " + activeArtifact.getName() + " has been added to your inventory.");
+				System.out.println("You got the " + activeArtifact.getName() + "! \n" +
+						activeArtifact.getName() + " has been added to your inventory.");
 			}
 			else
 			{
-				System.out.println("There's nothing here to pick up..");
+				System.out.println("There's nothing here to pick up...");
 			}
 		}
 		else if (command.equals("use") || command.equals("observe"))
@@ -86,51 +79,6 @@ public class CharacterSubsystem
 			{
 				System.out.println("I don't know where you can get one of those, but you definitely don't have one.");
 			}
-		}	
-	}
-
-	/**Method: fight
-	 * 
-	 */
-	public void fight(Zombie tempZom)
-	{
-		Scanner input = new Scanner(System.in);
-		String help = "Available fight commands: \nAttack, Flee";
-		
-		System.out.println("Fight engaged with " + tempZom.getName() + "!");
-		System.out.println(((Zombie) tempZom).getName() + " HP: " + tempZom.getHealth()		
-		+ "/" + tempZom.getMaxHealth());
-		System.out.println("Your HP: " + player.getHealth() + "/" + player.getMaxHealth());
-		System.out.println("\n" + help);
-		while (player.getHealth() > 0 && tempZom.getHealth() > 0)
-		{
-			System.out.print("> ");
-			if (input.nextLine().equals("attack"))
-			{
-				player.attack(tempZom);
-				if (tempZom.getHealth() > 0 && player.getHealth() > 0)
-				{
-					try
-					{
-						Thread.sleep(800);
-					} catch (InterruptedException e)
-					{
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-					tempZom.attack(player);
-
-				}
-				else if (tempZom.getHealth() <= 0)
-				{
-					System.out.println(tempZom.getName() + " is dead!");
-					System.out.println("Your HP: " + player.getHealth() + "/" + player.getMaxHealth());
-				}
-				else if (player.getHealth() <= 0)
-				{
-					System.out.println("You are dead.");
-				}
-			}
 		}
 	}
 
@@ -145,14 +93,14 @@ public class CharacterSubsystem
 		Artifact parsedArtifact;
 
 		//First letter of first word capital, rest of word lower-case
-		artifactInput = artifactInput.substring(0, 1).toUpperCase() 
+		artifactInput = artifactInput.substring(0, 1).toUpperCase()
 				+ artifactInput.substring(1, artifactInput.length()).toLowerCase();
 
 		if (artifactInput.contains("_"))
 		{
 			separatorIndex = artifactInput.indexOf("_");
 			//First letter of second word capital
-			artifactInput = artifactInput.substring(0, separatorIndex) + " " +  
+			artifactInput = artifactInput.substring(0, separatorIndex) + " " +
 					artifactInput.substring(separatorIndex + 1, separatorIndex + 2).toUpperCase() +
 					artifactInput.substring(separatorIndex + 2);
 			artifactInput = artifactInput.replaceAll("_", " ");
@@ -162,7 +110,7 @@ public class CharacterSubsystem
 		{
 			separatorIndex = artifactInput.indexOf("-");
 			//First letter after dash capital
-			artifactInput = artifactInput.substring(0, separatorIndex) + "-" +  
+			artifactInput = artifactInput.substring(0, separatorIndex) + "-" +
 					artifactInput.substring(separatorIndex + 1, separatorIndex + 2).toUpperCase() +
 					artifactInput.substring(separatorIndex + 2);
 		}
@@ -173,5 +121,37 @@ public class CharacterSubsystem
 			parsedArtifact = activeRoom.getArtifact();
 		}
 		return parsedArtifact;
+	}
+
+	/**
+	 * @return the player
+	 */
+	public static Player getPlayer()
+	{
+		return player;
+	}
+
+	/**
+	 * @return the activeRoom
+	 */
+	public Room getActiveRoom()
+	{
+		return activeRoom;
+	}
+
+	/**
+	 * @param player the player to set
+	 */
+	public static void setPlayer(Player player)
+	{
+		CharacterSubsystem.player = player;
+	}
+
+	/**
+	 * @param activeRoom the activeRoom to set
+	 */
+	public void setActiveRoom(Room activeRoom)
+	{
+		this.activeRoom = activeRoom;
 	}
 }
